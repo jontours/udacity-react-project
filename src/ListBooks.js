7e0a4d543da2aec769e7a4cace7fd1dc31caec3a
Jonathan Carmack <jonathan.carmack@gsa.gov>
2017-08-07 21:46:10 -0400

    feat: hooked up main page components to api

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -4,18 +4,20 @@ import Book from './Book';
 
 class ListBooks extends React.Component{
 	render() {
-		const {title, books} = this.props;
+		const {title, shelf, books} = this.props;
+
+		let filteredBooks = books.filter( (book) => book.shelf === shelf)
 		return(
+
 	        <div className="bookshelf">
 	          <h2 className="bookshelf-title">{title}</h2>
 	          <div className="bookshelf-books">
 	            <ol className="books-grid">
-	              <li>
-	                <Book/>
-	              </li>
-	              <li>
-	                <Book/>
-	              </li>
+	            	{filteredBooks.map((book) => (
+		              <li key={book.id}>
+		                <Book book_data={book}/>
+		              </li>
+		             ))}
 	            </ol>
 	          </div>
 	        </div>
